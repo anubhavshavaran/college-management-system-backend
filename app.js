58bb4d012f8e715726bccf7283aa4290da3b2245
Anubhav <anubhavshavaran5678@gmail.com>
2024-12-10 00:29:26 +0530

    Ad utils

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import AppError from "./utils/appError.js";
+import errorController from "./utils/ErrorController.js";
 
 const app = express();
 
@@ -8,6 +10,12 @@ app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Welcome to the Node App'
     });
+});
+
+app.use(errorController);
+
+app.all('*', (req, res, next) => {
+   next(new AppError(`Can't find ${req.originalUrl} on the server.`, '404'));
 });
 
 export default app;
