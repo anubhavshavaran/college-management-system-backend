b65d04fbcb25b71bec8c0c6238598cf463023efa
Anubhav <anubhavshavaran5678@gmail.com>
2025-01-02 18:05:09 +0530

    Changes in server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import app from "./app.js";
 import dotenv from 'dotenv';
 import mongoose from "mongoose";
-import express from "express";
 
 dotenv.config();
 
@@ -9,10 +8,8 @@ const port = process.env.PORT;
 const DB = process.env.DATABASE_URL;
 const DBNAME = process.env.DATABASE_NAME;
 
-mongoose.connect(DB, {
+await mongoose.connect(DB, {
     dbName: DBNAME
-}).then(() => {
-    console.log(`${DBNAME} connected successfully!`);
 });
 
 app.listen(port, () => {
