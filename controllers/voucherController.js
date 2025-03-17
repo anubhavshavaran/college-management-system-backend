1587b6608e3dc0170c8becbfdc9c954379e3a2a2
Anubhav Shavaran <anubhavshavaran@gmail.com>
2025-03-17 23:24:33 +0530

    Added a function in the vouchers controller to query vouchers with dates.

--- controllers/voucherController.js
+++ controllers/voucherController.js
@@ -1,10 +1,40 @@
 import {createOne, deleteOne, getAll, getOne, updateOne} from "./handlerFactory.js";
 import Voucher from "../models/voucherModel.js";
+import catchAsync from "../utils/CatchAsync.js";
 
-const getAllVouchers = getAll(Voucher);
 const getVoucher = getOne(Voucher);
 const createVoucher = createOne(Voucher);
 const updateVoucher = updateOne(Voucher);
 const deleteVoucher = deleteOne(Voucher);
+
+const getAllVouchers = catchAsync(async (req, res) => {
+    let query = {};
+
+    const {start, end, date} = req.query;
+
+    if (date && !start && !end) {
+        query = {
+            date
+        }
+    } else if (!date && start && end) {
+        query = {
+            date: {
+                $gte: start,
+                $lte: end,
+            }
+        }
+    }
+
+    const docs = await Voucher.find({
+        ...query,
+        organization: req.params.organization.toUpperCase(),
+    });
+
+    res.status(200).json({
+        status: 'success',
+        length: docs.length,
+        data: {docs},
+    });
+});
 
 export {getAllVouchers, getVoucher, createVoucher, updateVoucher, deleteVoucher};
